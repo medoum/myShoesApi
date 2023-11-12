7f14457912b572375557ff5d7e3cdb00c898b062
medoum <mohamedoum2430@gmail.com>
2023-11-12 12:27:51 +0100

    Initial commit

---
+++ src/main/java/com/example/myshoesapi/MyShoesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.myshoesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyShoesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyShoesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myshoesapi/MyShoesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myshoesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyShoesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
